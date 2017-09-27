64af8b782c36633f3a70773ddd414d59b98dadef
Cato Myhre <boredgamerz1998@gmail.com>
2017-09-27 11:26:18 +0200

    Logging partly fixed
    issue #2 partly fixed.
    
    fixed in this commit:
    + The log file are finally being created properly
    + The log file no longer gets deleted when the script is finished
    
    Things that still needs to be fixed:
    The script does check  the log, but ignores if the path does exist in
    the log file

--- RunScriptFromServer.ps1
+++ RunScriptFromServer.ps1
@@ -8,8 +8,8 @@
     
     $doesLogFileExist = Test-Path -Path $LogFile 
 
-    if($doesLogFileExist -eq $false){
-        New-Item -Path $LogFile -Type File
+    if ($doesLogFileExist -eq $false) {
+        New-Item -Path $LogFile -Type File -Force
     }
 
     if ($testGivenPath -eq $true ) {
@@ -19,12 +19,22 @@
         foreach ($children in $allChildren) {
             $ScriptPath = $ScriptParentFolder + $children.Name
             $FullPath = $ScriptPath + "\" + $ScriptName
-            #Write-Output "Full Script path: " + $FullPath
+            Write-Output "Full Script path: " + $FullPath
             $hasScriptRunned = Get-Content $LogFile
-            foreach ($line in $hasScriptRunned) {
-                if ($line -ne $FullPath) {
-                    Invoke-Expression $FullPath
-                    Add-Content $LogFile -Value $FullPath
+            if ($hasScriptRunned.Length -eq 0) {
+                Write-Output "Log is empty"
+                Invoke-Expression $FullPath
+                Add-Content $LogFile -Value $FullPath
+            }
+            else {
+                foreach ($line in $hasScriptRunned) {
+                    if ($line -ne $FullPath) {
+                        Write-Output $FullPath
+                        #Invoke-Expression $FullPath
+                        #Add-Content $LogFile -Value $FullPath                        
+                    }else{
+                        Write-Output "Script is equal"
+                    }
                 }
             }
         }
@@ -33,4 +43,14 @@
     else {Write-Output "The Path: '$($PATH)' does not exist"}
 }
 
+function readLogFile(){
+    $ReadLogFile = Get-Content -Path "C:\logs\testLog.txt"
+
+    foreach($line in $ReadLogFile){
+        Write-Output $line
+    }
+
+}
+
 runScriptFromServer
+#readLogFile
