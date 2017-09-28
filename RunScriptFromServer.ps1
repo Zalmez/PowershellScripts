a335add623c24e3f985b767fb577bdbd9c2a53bb
Cato Myhre <boredgamerz1998@gmail.com>
2017-09-28 09:28:24 +0200

    Logic changed slightly
    I've tried to limit when paths will be runned in the loop.
    
    So far I have figured out why the designated scripts are running
    multiple times. and I might have a solution for the issue #2

--- RunScriptFromServer.ps1
+++ RunScriptFromServer.ps1
@@ -3,53 +3,60 @@
     [string]$ScriptName = "test_Script.ps1"
     [string]$LogFile = "C:\logs\testLog.txt"
     [string]$LogActionFile = "C:\ScriptLogs\ActionLog.txt"
-
-    $testGivenPath = Test-Path -path $ScriptParentFolder
+    $testGivenPath = testPath($ScriptParentFolder)
     
-    $doesLogFileExist = Test-Path -Path $LogFile 
+    $doesLogFileExist = testPath($LogFile)
+
+    $hasScriptRunned = Get-Content $LogFile
 
     if ($doesLogFileExist -eq $false) {
         New-Item -Path $LogFile -Type File -Force
     }
 
     if ($testGivenPath -eq $true ) {
-        Write-Output  "Path: '$($ScriptParentFolder)'  exists!"
+        #Write-Output  "Path: '$($ScriptParentFolder)'  exists!"
         $allChildren = Get-ChildItem -path $PATH -Recurse -Directory | Select-Object Name
             
         foreach ($children in $allChildren) {
             $ScriptPath = $ScriptParentFolder + $children.Name
             $FullPath = $ScriptPath + "\" + $ScriptName
-            Write-Output "Full Script path: " + $FullPath
-            $hasScriptRunned = Get-Content $LogFile
-            if ($hasScriptRunned.Length -eq 0) {
-                Write-Output "Log is empty"
-                Invoke-Expression $FullPath
-                Add-Content $LogFile -Value $FullPath
-            }
-            else {
-                foreach ($line in $hasScriptRunned) {
-                    if ($line -ne $FullPath) {
-                        Write-Output $FullPath
-                        #Invoke-Expression $FullPath
-                        #Add-Content $LogFile -Value $FullPath                        
-                    }else{
-                        Write-Output "Script is equal"
+            write-Output "Full Script path: " + $FullPath
+            if (testPath($FullPath) -eq $true) {
+                if ($hasScriptRunned.Length -eq 0) {
+                    Write-Output "Log is empty"
+                    Invoke-Expression $FullPath
+                    Add-Content $LogFile -Value $FullPath
+                }
+                else {
+                    foreach ($line in $hasScriptRunned) {
+                        if ($FullPath -ne $line) {
+                            #Write-Output $FullPath
+                            Invoke-Expression $FullPath
+                            Add-Content $LogFile -Value $FullPath                        
+                        }
+                        else {
+                            Write-Output "Script is equal"
+                        }
                     }
                 }
-            }
-        }
-    
+            }else{Write-Output "Ignoring folder. Script not found"}
+        
+        }    
     }
     else {Write-Output "The Path: '$($PATH)' does not exist"}
 }
 
-function readLogFile(){
+function readLogFile() {
     $ReadLogFile = Get-Content -Path "C:\logs\testLog.txt"
 
-    foreach($line in $ReadLogFile){
+    foreach ($line in $ReadLogFile) {
         Write-Output $line
     }
 
+}
+
+function testPath ([string]$pathToTest) {
+    Test-Path -Path $pathToTest
 }
 
 runScriptFromServer
