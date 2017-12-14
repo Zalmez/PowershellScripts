747c631f26d0c97b909a68bd55cfe2264eb65884
Cato Myhre <boredgamerz1998@gmail.com>
2017-12-14 13:39:46 +0100

    Added action logging
    Change Log:
    * Moved when contents of  the log file where read
    * Changed the $hasScriptRunned variable to $logFilecontents
    + Added a action logging method
    + Added a logging method

--- RunScriptFromServer.ps1
+++ RunScriptFromServer.ps1
@@ -1,18 +1,26 @@
 ﻿Function runScriptFromServer() {
-    [string]$ScriptParentFolder = "C:\Test\" #Hoved mappe(aka ParentFolder)
-    [string]$ScriptName = "test_Script.ps1"
+    [string]$ScriptParentFolder = "M:\pc\Dokumenter\GitHub\PowershellScripts" #Hoved mappe(aka ParentFolder)
+    [string]$ScriptName = "test_script.ps1"
     [string]$LogFile = "C:\logs\testLog.txt"
-    [string]$LogActionFile = "C:\ScriptLogs\ActionLog.txt"
+    [string]$LogActionFile = "C:\logs\ActionLog.txt"
     $testGivenPath = testPath($ScriptParentFolder)
-    
+
+    $doesActionLogFileExist = testPath($LogActionFile)
     $doesLogFileExist = testPath($LogFile)
 
     # Noen av disse variabelnavnene er litt forvirrende. Denne burde kanskje hete $logFileContents eller noe sånt (for meg ser det ut som at det burde være en boolean)
-    $hasScriptRunned = Get-Content $LogFile
-
+    
+    if($doesActionLogFileExist -eq $false){
+        New-Item -Path $LogActionFile -Type File -Force
+        writeAction("Action file did not exist, created Action Log File")
+    }
+    
     if ($doesLogFileExist -eq $false) {
         New-Item -Path $LogFile -Type File -Force
+        writeAction("Log file does not exist! Created Log File")
     }
+
+    $logFileContents = Get-Content $LogFile
 
     if ($testGivenPath -eq $true ) {
         #Write-Output  "Path: '$($ScriptParentFolder)'  exists!"
@@ -23,11 +31,11 @@
             $hasCurrentScriptRan = $false
             $ScriptPath = $ScriptParentFolder + $children.Name
             $FullPath = $ScriptPath + "\" + $ScriptName
-            write-Output "Full Script path: " + $FullPath
+            #write-Output "Full Script path: " + $FullPath
             # ... sjekke om skriptet faktisk finnes ...
             if (testPath($FullPath) -eq $true) {                                
                 # Gå gjennom hver linje i loggfila (merk at du ikke trenger å ta spesiell høyde for at fila ikke inneholder noen linjer -- det betyr bare at denne løkka ikke kjører)
-                foreach ($line in $hasScriptRunned) {                        
+                foreach ($line in $logFileContents) {                        
                     if ($FullPath -eq $line) {                            
                         # Hvis vi finner en linje som matcher navnet på skriptet så har det blitt kjørt fra før. Sett et flagg og stopp.
                         $hasCurrentScriptRan = $true                        
@@ -38,14 +46,14 @@
                 # Når vi har gått gjennom alle linjene kan vi se om flagget har blitt satt, hvis ikke kjører vi skriptet.
                 if (! $hasCurrentScriptRan) { # (! $hasCurrentScriptRan) tilsvarer ($hasCurrentScriptRan -eq $false)
                     Invoke-Expression $FullPath
-                    Add-Content $LogFile -Value $FullPath
+                    writeLog($FullPath)
                 }
-            } else {
-                Write-Output "Ignoring folder. Script not found"
-            }        
+            } #else {
+               # writeAction("Script not found. Ignoring folder")
+            #}        
         }    
     } else {
-        Write-Output "The Path: '$($PATH)' does not exist"
+        writeAction("The Path: '$($PATH)' does not exist")
     }
 }
 
@@ -60,6 +68,17 @@ function readLogFile() {
 
 function testPath ([string]$pathToTest) {
     Test-Path -Path $pathToTest
+}
+
+function writeAction($strValue){
+    [string]$LogActionFile = "C:\logs\ActionLog.txt"
+    $Date = Get-Date
+    Add-Content -Path $LogActionFile -Value "[($Date)]: ($strValue)"
+}
+
+function writeLog($strValue){
+    [string]$LogFile = "C:\logs\testLog.txt"
+    Add-Content -Path $LogFile -Value "($strValue)"
 }
 
 runScriptFromServer
